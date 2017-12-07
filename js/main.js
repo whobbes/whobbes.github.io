9ce0187e3a84485a6bd287695adf3d9dcde4ddba
Wallace <wallacehobbes@gmail.com>
2017-12-07 23:42:03 +0000

    menu fixed. broken on mobile for now. other bits and bobs

--- js/main.js
+++ js/main.js
@@ -1,7 +1,19 @@
+// Animations for the page
+
+// Scrolling
 $("#landing").on('click', 'a[href^="#"]', function (event) {
     event.preventDefault();
 
     $('html, body').animate({
         scrollTop: $($.attr(this, 'href')).offset().top
     }, 1000);
+});
+
+// Menu background show
+$(window).on("scroll", function() {
+    if($(window).scrollTop() > 300) {
+        $(".navbar").addClass("active");
+    } else {
+       $(".navbar").removeClass("active");
+    }
 });
